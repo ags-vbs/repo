540f1b34ca86429e73f52bd6c9f3ef5d01be2d63
Augustinas V <augustinas@augustinas-ThinkPad-Edge-E535.(none)>
2013-12-15 13:31:17 +0200

    HomePage with about page instruction

--- vgtu-web/src/main/java/com/mycompany/vgtu/pages/home/HomePage.java
+++ vgtu-web/src/main/java/com/mycompany/vgtu/pages/home/HomePage.java
@@ -2,18 +2,27 @@ package com.mycompany.vgtu.pages.home;
 
 import com.mycompany.vgtu.page.layout.VgtuLayoutPage;
 import com.mycompany.vgtu.pages.navbar.MenuItemEnum;
-import org.apache.wicket.markup.html.panel.EmptyPanel;
+import com.mycompany.vgtu.utils.MyWicketMessages;
+import org.apache.wicket.markup.html.basic.Label;
 
 public class HomePage extends VgtuLayoutPage {
 
     private static final long serialVersionUID = 1L;
 
+    private final MyWicketMessages messages = MyWicketMessages.from(this);
+
     public HomePage() {
-        getContentContainer().setComponent(new EmptyPanel(getContentContainer().getId()));
+        getContentContainer().setComponent(getContentLabel(getContentContainer().getId()));
     }
 
     @Override
     public MenuItemEnum getActiveMenu() {
         return MenuItemEnum.NONE;
+    }
+
+    private Label getContentLabel(String wicketId) {
+        Label label = new Label(wicketId, messages.txtModel("infoAboutSiteLabel"));
+        label.setEscapeModelStrings(false);
+        return label;
     }
 }
